f3057b8ec14023313f8ae16981640ead7396c628
corolima3 <corolimajose@outlook.com>
2023-11-16 19:15:12 -0300

    jojo

--- api/index.js
+++ api/index.js
@@ -26,6 +26,6 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
