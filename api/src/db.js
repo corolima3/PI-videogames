2da0b083a9860e6f27a280ad72421c6e4f2ca353
corolima3 <corolimajose@outlook.com>
2023-11-16 18:22:17 -0300

    hi

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, API_KEY } = process.env;
-console.log(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`)
 
-//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-const sequelize = new Sequelize(`postgres://postgres:caracol2345@localhost/videogames`, {
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
